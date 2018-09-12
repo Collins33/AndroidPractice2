baf38b2cfae69626538ff052b65598eae9e2d865
Collins Njau <collinsnjau39@gmail.com>
2018-09-12 21:45:27 +0300

    Broadcast the typing event to all other sockets

--- public/index.js
+++ public/index.js
@@ -29,5 +29,10 @@ btn.addEventListener('click', function(){
 
 // listen for events on the server
 socket.on('chat', function(data){
+    feedback.innerHTML=""
     output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing', function(data){
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
 });
