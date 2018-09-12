d952885411a6f8811b0587eba72fa81e99bfcfad
Collins Njau <collinsnjau39@gmail.com>
2018-09-12 21:14:43 +0300

    Send message back to the server through the socket

--- public/index.js
+++ public/index.js
@@ -1,2 +1,23 @@
 // Make connection
 var socket = io.connect('http://localhost:5000');
+
+// Query DOM
+var message = document.getElementById('message'),
+      handle = document.getElementById('handle'),
+      btn = document.getElementById('send'),
+      output = document.getElementById('output');
+
+// emit event when someone clicks send
+btn.addEventListener('click', function(){
+    //send message to server via the socket
+
+    // it will emit a message down the socket to the
+    // server
+    // It takes two parameters:
+    // name of message
+    // what the actual data will be
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    })
+})
